b6f495e4d9ba90fa61a645390119af99a590cd85
Manika Kewat <manikewat0108@gmail.com>
2026-06-17 02:32:49 +0530

    Time: 1 ms (73.68%) | Memory: 10 MB (71.01%) - LeetSync

---
+++ 567-permutation-in-string/permutation-in-string.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) 
+    {int n = s1.size();
+    int m = s2.size();
+    if( n >m )
+        return false;
+
+        vector<int> freq1(26,0);
+        vector<int> freq2(26,0);
+
+        for( char c :s1)
+        freq1[ c - 'a']++;
+
+        for( int j = 0; j < m ; j++){
+            freq2[ s2[j] - 'a']++;
+
+            if( j >= n)
+            freq2[s2[j-n] - 'a']--;
+            if( freq1 == freq2)
+            return true ;
+        }
+    
+        return false;
+    }
+};

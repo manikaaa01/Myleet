a081131ecede69c86de6ded7e0fd46326553875c
Manika Kewat <manikewat0108@gmail.com>
2026-06-22 19:38:04 +0530

    Time: 0 ms (100.00%) | Memory: 13 MB (16.54%) - LeetSync

---
+++ 682-baseball-game/baseball-game.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int calPoints(vector<string>& operations) {
+        stack<int> st;
+        for( auto op: operations){
+
+            if( op == "C"){
+                st.pop();
+            }
+            else if( op == "D"){
+                st.push( 2 * st.top());
+            }
+            else if( op == "+"){
+                int a = st.top();
+                st.pop();
+                 int b = st.top();
+
+                 st.push(a);
+                 st.push(a+b);
+            }
+            else{
+                st.push(stoi(op));
+            }
+        }
+        int sum = 0;
+        while(!st.empty()){
+            sum+= st.top();
+            st.pop();
+        }
+        return sum;
+    }
+};

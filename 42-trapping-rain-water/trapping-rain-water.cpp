d198c8ef2a9bfe2d297fef71af60f560c61690c5
Manika Kewat <manikewat0108@gmail.com>
2026-06-27 23:39:52 +0530

    Time: 1 ms (23.23%) | Memory: 27 MB (37.32%) - LeetSync

---
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        stack<int> st;
+        int water = 0;
+        for(int i = 0; i<height.size(); i++){
+            while(!st.empty() && height[i] > height[st.top()]){
+                int bottom = st.top();
+                st.pop();
+
+                if(st.empty())
+                break;
+                int left = st.top();
+                int width = i-left - 1;
+                int h = min( height[left], height[i]) - height[ bottom ];
+                water += width * h;
+            }
+            st.push(i);
+
+        }
+        return water;
+    }
+};
